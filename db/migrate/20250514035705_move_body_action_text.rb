701e34c4af223f0b668e93386ee3bbcbaac4b509
Jahon A <amjahongir@gmail.com>
2025-05-13 21:47:20 -0700

    Add rich text

---
+++ db/migrate/20250514035705_move_body_action_text.rb
@@ -0,0 +1,13 @@
+class MoveBodyActionText < ActiveRecord::Migration[7.1]
+
+  def change
+
+    BlogPost.all.find_each do |blog_post|
+      blog_post.update(content: blog_post.body)
+    end
+
+    remove_column :blog_posts, :body
+
+  end
+
+end
